f2727a494b9b61ac3050f299d00c8c30eb4a0fe8
Jack Mordaunt <jackmordaunt@gmail.com>
2018-04-26 21:24:59 +1200

    [+] Allow user to specify bounds.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,29 @@
 extern crate rand;
 
+use std::env::args;
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut lower: u32 = 1;
+    let mut upper: u32 = 100;
+    for arg in args() {
+        let bounds: Vec<&str> = arg.split("-").collect();
+        if bounds.len() == 2 {
+            lower = match bounds[0].trim().parse() {
+                Ok(num) => num,
+                Err(_) => 1,
+            };
+            upper = match bounds[1].trim().parse() {
+                Ok(num) => num,
+                Err(_) => 100,
+            };
+            break;
+        }
+    }
+    println!("Guess the number between {} and {}.", lower, upper);
+    let secret_number = rand::thread_rng().gen_range(lower, upper+1);
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
