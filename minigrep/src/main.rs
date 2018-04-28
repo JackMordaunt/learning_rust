0e43bf64e7c9f74176361879948a58ac4a8cf411
Jack Mordaunt <jackmordaunt@gmail.com>
2018-04-28 22:06:17 +1200

    [+] Read file contents.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs::File;
+// prelude contains useful traits for doing io.
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let path = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", path);
+    let mut f = File::open(path).expect("could not open file");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("could not read the file");
+    println!("With text:\n{}", contents);
 }
